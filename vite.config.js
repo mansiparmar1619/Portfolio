777d0ce3841ac39159ba958e8a3b706687445823
Mansi Parmar <mansiparmar1619@gmail.com>
2026-01-29 01:55:55 +0530

    Deploy production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  
+  base: '/Portfolio/',
+
+
   server: {
     port: 3000,
     proxy: {
@@ -14,4 +19,3 @@ export default defineConfig({
     }
   }
 })
-
